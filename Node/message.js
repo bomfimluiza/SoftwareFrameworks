8f7976152d5434940a0dc710e508862723736c86
bomfimluiza <bomfimluiza@gmail.com>
2018-03-07 19:48:59 -0300

    Message changed

--- Node/message.js
+++ Node/message.js
@@ -1,3 +1,3 @@
 exports.send = function(){
-    return "<h1 style='color: magenta; text-align: center;'>Luiza Bomfim</h1>";
+    return "<div style='float: left; width: 40%'><hr style='border-color: magenta; margin-top: 40px;'></div><div><h1 style='color: magenta; text-align: center; float: left; width: 20%'>Luiza Bomfim</h1></div><div style='float: left; width: 40%'><hr style='border-color: magenta; margin-top: 40px;'></div>";
 }
