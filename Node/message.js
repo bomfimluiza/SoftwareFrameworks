97d1c4b32e0c1b7d2e36447a3c85c89108f9b4dc
bomfimluiza <bomfimluiza@gmail.com>
2018-03-07 17:34:45 -0300

    Exporting message from another file

---
+++ Node/message.js
@@ -0,0 +1,3 @@
+exports.send = function(){
+    return "<h1 style='color: magenta; text-align: center;'>Luiza Bomfim</h1>";
+}
--- Node/myfirst.js
+++ Node/myfirst.js
@@ -1,8 +1,9 @@
 var http = require("http");
+var message = require("./message.js");
 
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
-    res.end("<h1 style='color: magenta;'>Luiza Bomfim</h1>");
+    res.end(message.send());
 }).listen(3000);
 
 console.log("Xôu");
